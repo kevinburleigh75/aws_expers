6ce52ed1d15d733519506c72c8a67e34966e5dcc
Kevin Burleigh <klb@kindling>
2017-01-30 14:33:20 -0600

    add timing output

--- exper3/use_cpu.rb
+++ exper3/use_cpu.rb
@@ -1,5 +1,14 @@
+num_per_print = 10_000_000
+
+old_time = Time.now()
+
 counter = 0
 loop do
   counter += 1
-  puts "counter = #{counter}" if counter % 10_000_000 == 0
+
+  if counter % num_per_print == 0
+    new_time = Time.now
+    puts "counter = #{counter} #{sprintf('%1.3e', num_per_print/(new_time-old_time))} loops/sec"
+    old_time = new_time
+  end
 end
